63949db0f84077bcc3c8c831513007668d098f80
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-27 11:55:52 +0530

    react

---
+++ frontend/cosomofeed/src/component/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+    <div>Login</div>
+     <p>Email</p>
+     <p>Password</p>
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/cosomofeed/src/component/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+    <div>signup</div>
+    <div>
+       <p>Name</p>
+       <p>Email</p>
+       <p>Password</p>
+    </div>
+    </div>
+  )
+}
+
+export default Signup
