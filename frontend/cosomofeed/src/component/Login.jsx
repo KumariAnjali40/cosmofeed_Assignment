440595e7cb9d07a1a50253fa6ce5ee4cf1d7a64c
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-27 12:00:26 +0530

    signup

--- frontend/cosomofeed/src/component/Login.jsx
+++ frontend/cosomofeed/src/component/Login.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
     <div>
-    <div>Login</div>
+    <div className='maindiv'>
      <p>Email</p>
      <p>Password</p>
+     <button>Login</button>
+    </div>
     </div>
   )
 }
--- frontend/cosomofeed/src/component/Signup.jsx
+++ frontend/cosomofeed/src/component/Signup.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Signup = () => {
   return (
     <div>
-    <div>signup</div>
+    {/* <div>signup</div> */}
     <div>
        <p>Name</p>
        <p>Email</p>
        <p>Password</p>
+       <button>Signup</button>
     </div>
     </div>
   )
